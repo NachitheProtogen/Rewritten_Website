ae5614a7a2098ccee11f2f20bf2aa6ecf66d189a
Nachi <nils.christian2006@gmail.com>
2024-06-05 13:55:23 +0200

    Update

---
+++ JS/main.js
@@ -0,0 +1,9 @@
+let main = document.getElementById("mainframe")
+
+home.addEventListener("click", () => {
+    main.setAttribute("src", "HTML/main.html");
+})
+
+art.addEventListener("click", () => {
+    main.setAttribute("src", "HTML/artcorner.html");
+})
