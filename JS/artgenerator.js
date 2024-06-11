f2a7f3675abe63167814ab3473f9afed1976ee1e
Nachi <nils.christian2006@gmail.com>
2024-06-11 11:11:14 +0200

    deb

--- JS/artgenerator.js
+++ JS/artgenerator.js
@@ -1,7 +1,6 @@
 function CreateSingle() { //Creates a single frame for an Artpiece
     const images = [
-        {file: "../IMG/30.12_-_shadow_nachi_animation.gif", width: "200", artist: "Kir0nixx_", credit: "https://x.com/Kir0nixx_"},
-        {file: "../IMG/2_-_proto_png.png", width: "200", artist: "Idrael", credit: "https://idrael.carrd.co/"},
+        
     ];
     images.forEach(image => {
         let pic = document.createElement("img");
@@ -22,9 +21,46 @@ function CreateSingle() { //Creates a single frame for an Artpiece
 }
 
 function createColection() { //Creates a Collection of Images in a single frame
-    
+    const images = [
+        {file: "../IMG/2_-_proto_png.png", width: "200", artist: "Idrael", credit: "https://idrael.carrd.co"},
+        {file: "../IMG/30.12_-_shadow_nachi_animation.gif", width: "200", artist: "Kir0nixx_", credit: "https://x.com/Kir0nixx_"},
+        {file: "../IMG/06.12_-_shadow_nachi_ref.png", width: "200", artist: "Kir0nixx_", credit: "https://x.com/Kir0nixx_"},
+    ]
+
+    const groupedByArtist = {};
+
+    // Group images by artist
+    images.forEach(image => {
+        if (!groupedByArtist[image.artist]) {
+            groupedByArtist[image.artist] = {
+                credit: image.credit,
+                images: []
+            };
+        }
+        groupedByArtist[image.artist].images.push(image.file);
+    });
+    for (const artist in groupedByArtist) {
+        const artistInfo = groupedByArtist[artist];
+
+        let frame = document.createElement("div");
+        frame.className = "container";
+
+        artistInfo.images.forEach(image => {
+            let pic = document.createElement("img");
+            pic.src = image.file;
+            pic.width = image.width;
+            frame.appendChild(pic);
+        });
+
+        let des = document.createElement("p");
+        des.innerHTML = `This was created by <a href="${artistInfo.credit}">${artist}</a>!`;
+        frame.appendChild(des);
+
+        artdiv.appendChild(frame);
+    }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
     CreateSingle();
+    createColection();
 })
