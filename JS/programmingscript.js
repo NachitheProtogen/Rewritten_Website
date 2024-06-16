6fffc2701324c07e61720ae119304dc36f3e7f95
NachitheProtogen <nils.christian2006@gmail.com>
2024-06-16 15:45:15 +0200

    hope this works

--- JS/programmingscript.js
+++ JS/programmingscript.js
@@ -1,9 +1,9 @@
 let main = parent.document.getElementById("mainframe");
 
 $(paint).click(function () { 
-    $(main).attr("src", "../legacy/HTML/art.html");
+    $(main).attr("src", "../old/HTML/art.html");
 });
 
 $(life).click(function () {
-    $(main).attr("src", "../legacy/HTML/life.html");
+    $(main).attr("src", "../old/HTML/life.html");
 })
